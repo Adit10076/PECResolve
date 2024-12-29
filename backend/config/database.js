f45fd368da6fa4e6602e50ea0106fc9cc1d49996
Adit Arora <aditarora71@gmail.com>
2024-12-29 16:12:53 +0530

    set config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
+
 const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL)
-    .then(()=>{
-        console.log("Database connected successfully");
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .catch((e)=>{
-        console.log("database connection unsuccessfull");
+    .then(() => {
+      console.log("Database connected successfully");
     })
+    .catch((error) => {
+      console.error("Database connection failed:", error);  // Log the error
+      process.exit(1);  // Exit the process if the database connection fails
+    });
 };
-module.exports=dbConnect;
+
+module.exports = dbConnect;
